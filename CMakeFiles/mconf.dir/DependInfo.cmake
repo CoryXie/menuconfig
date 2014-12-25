1eeaf4adaabbaa3c2c5fe3038f2b8afc0c9fb440
CoryXie <cory.xie@gmail.com>
2014-12-25 09:06:09 -0800

    run cmake to generate Makefile for default use

---
+++ CMakeFiles/mconf.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/coryxie/projects/v64/host/menuconfig/mconf.c" "/home/coryxie/projects/v64/host/menuconfig/CMakeFiles/mconf.dir/mconf.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/coryxie/projects/v64/host/menuconfig/lxdialog/CMakeFiles/lxdialog.dir/DependInfo.cmake"
+  "/home/coryxie/projects/v64/host/menuconfig/CMakeFiles/zconf.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "lxdialog"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ lxdialog/CMakeFiles/lxdialog.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/coryxie/projects/v64/host/menuconfig/lxdialog/checklist.c" "/home/coryxie/projects/v64/host/menuconfig/lxdialog/CMakeFiles/lxdialog.dir/checklist.c.o"
+  "/home/coryxie/projects/v64/host/menuconfig/lxdialog/inputbox.c" "/home/coryxie/projects/v64/host/menuconfig/lxdialog/CMakeFiles/lxdialog.dir/inputbox.c.o"
+  "/home/coryxie/projects/v64/host/menuconfig/lxdialog/menubox.c" "/home/coryxie/projects/v64/host/menuconfig/lxdialog/CMakeFiles/lxdialog.dir/menubox.c.o"
+  "/home/coryxie/projects/v64/host/menuconfig/lxdialog/textbox.c" "/home/coryxie/projects/v64/host/menuconfig/lxdialog/CMakeFiles/lxdialog.dir/textbox.c.o"
+  "/home/coryxie/projects/v64/host/menuconfig/lxdialog/util.c" "/home/coryxie/projects/v64/host/menuconfig/lxdialog/CMakeFiles/lxdialog.dir/util.c.o"
+  "/home/coryxie/projects/v64/host/menuconfig/lxdialog/yesno.c" "/home/coryxie/projects/v64/host/menuconfig/lxdialog/CMakeFiles/lxdialog.dir/yesno.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "lxdialog"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
